43947b51f1ee6a36ddf53b690c798454e79f9f70
ankuram9564 <123427044+ankuram9564@users.noreply.github.com>
2026-07-09 11:26:13 +0530

    Refactor product handling and redirect to products page
    Removed product management functionality and redirected to products page.

--- app.js
+++ app.js
@@ -1,31 +1,5 @@
-let products = JSON.parse(localStorage.getItem("products")) || [];
-
 function openProducts() {
-  let name = prompt("Enter Product Name:");
-
-  if (!name) return;
-
-  products.push({
-    name: name
-  });
-
-  localStorage.setItem("products", JSON.stringify(products));
-
-  showProducts();
+    window.location.href = "products.html";
 }
 
-function showProducts() {
-  let list = "Products List\n\n";
-
-  if (products.length === 0) {
-    list += "No Products";
-  } else {
-    products.forEach((p, i) => {
-      list += (i + 1) + ". " + p.name + "\n";
-    });
-  }
-
-  alert(list);
-}
-
-console.log("YmoKart Started");
+console.log("YmoKart Business Manager Started");
